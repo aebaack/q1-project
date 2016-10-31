4a083f49ec83b0ecc67d4b7e070e691d518250d4
Aidan Baack <aebaack@gmail.com>
2016-10-31 11:14:22 -0600

    Create select button next to each poem with click event

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 'use strict';
 $("#searchPoems").on("click", function() {
-  // var poemForm = $("form[name='poems']").serializeArray();
   var poemForm = document.forms["poems"];
   var poet = poemForm["poet"].value;
   var title = poemForm["title"].value;
@@ -15,7 +14,7 @@ $("#searchPoems").on("click", function() {
       for (var i = 0; i < data.length; i++) {
         var li = $("<li>");
         var divHeader = $("<div>"), divBody = $("<div>");
-        divHeader.attr("class", "collapsible-header").text(data[i].title);
+        divHeader.attr("class", "collapsible-header").html(data[i].title + '<a class="waves-effect waves-teal btn-flat poemSelector">Select</a>');
         divBody.attr("class", "collapsible-body white");
         var poemLines = "";
         for (var j = 0; j < data[i].lines.length; j++) {
@@ -27,9 +26,14 @@ $("#searchPoems").on("click", function() {
       }
       $(".preloader-wrapper").attr("class", "hide");
       $("#loadingPoems").text("Select a Poem");
+      $(".poemSelector").on("click", selectPoem);
     });
     poem.fail(function(err) {
       console.log(err);
     });
   }
 });
+
+function selectPoem(event) {
+  console.log(event);
+}
