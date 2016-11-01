1a635d6d6d57ab7c8e09efc20998486672009e89
Aidan Baack <aebaack@gmail.com>
2016-11-01 15:23:26 -0600

    Change Watson calls to have correct number of stanzas

--- js/color.js
+++ js/color.js
@@ -6,20 +6,22 @@ $(document).ready(function() {
 
   var stanzas = [], stanza = "", analyzeText = "";
   for (var i = 0; i < poem.lines.length; i++) {
-    if (poem.lines[i] === "" && i > 0) {
+    if (poem.lines[i] === "" && stanza !== "") {
       stanzas.push(stanza);
       stanza = "";
+      analyzeText+=". ";
       continue;
     }
     stanza += poem.lines[i] + "<br>";
-    analyzeText += poem.lines[i] + "\n";
+    analyzeText += poem.lines[i].replace("!", " ").replace(".", " ").replace("?"," ") + " ";
   }
-  if (stanzas.length === 0) {
-    stanzas.push(stanza);
-  }
+  stanzas.push(stanza);
+  analyzeText+=". ";
+  console.log(stanzas);
+  console.log(analyzeText);
 
   // var tones = $.get("https://g-watson-aidanbaack.herokuapp.com/?text=" + analyzeText);
-  var tones = $.get("http://poetdb.herokuapp.com/author/Shakespeare")
+  var tones = $.get("http://poetdb.herokuapp.com/author/Shakespeare");
 
   var poemStanza = $("#stanza"); // Poem stanza location
   poemStanza.hide(); // Hidden so that it can fade in later
@@ -60,7 +62,7 @@ $(document).ready(function() {
       "Fear": "#111805",
       "Disgust": "#9b9b79",
       "Joy": "#939d7d"
-    }
+    };
 
     var tempJSON = {"particles":{"number":{"value":24,"density":{"enable":true,"value_area":800}},"color":{"value":colors[strongestTone[0]]},"shape":{"type":"polygon","stroke":{"width":0,"color":"#000"},"polygon":{"nb_sides":6},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.3,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":31.565905665290902,"random":false,"anim":{"enable":true,"speed":10,"size_min":40,"sync":false}},"line_linked":{"enable":false,"distance":200,"color":"#ffffff","opacity":1,"width":2},"move":{"enable":true,"speed":8,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"roittateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"grab"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true};
     particlesJS('particles-js', tempJSON);
