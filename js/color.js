026a756f83ca31b77cbb2baca996f5a04f65a5bc
Aidan Baack <aebaack@gmail.com>
2016-11-02 15:16:22 -0600

    Add ability to select multiple background colors

--- js/color.js
+++ js/color.js
@@ -44,7 +44,7 @@ $(document).ready(function() {
     // stanzaToneList is an array of tone objects for each stanza
     stanzaToneList = toneData["sentences_tone"];
 
-    // Background colors for each tone
+    // Particles colors for each tone
     var colors = {
       "Sadness": "#283593",
       "Anger": "#b71c1c",
@@ -52,10 +52,6 @@ $(document).ready(function() {
       "Disgust": "#6a1b9a",
       "Joy": "#ffee58"
     };
-
-    console.log(stanzas);
-    console.log(toneData);
-    console.log(stanzaToneList);
 
     // Create particles JSON
     var tempJSON = {"particles":{"number":{"value":24,"density":{"enable":true,"value_area":800}},"color":{"value":colors[strongestDocTone]},"shape":{"type":"polygon","stroke":{"width":0,"color":"#000"},"polygon":{"nb_sides":6},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.3,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":31.565905665290902,"random":false,"anim":{"enable":true,"speed":10,"size_min":40,"sync":false}},"line_linked":{"enable":false,"distance":200,"color":"#ffffff","opacity":1,"width":2},"move":{"enable":true,"speed":8,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"roittateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"grab"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true};
@@ -104,14 +100,20 @@ function strongestTone (toneData) {
 function changeBackground(tone) {
   // Changes background color to the given tone
   var colorBackground = {
-    "Sadness": "#2c2e43",
-    "Anger": "#550808",
-    "Fear": "#1e2d12",
-    "Disgust": "#9b9b79",
-    "Joy": "#939d7d"
+    "Sadness": ["#2c2e43", "#21277F", "#353FCC"],
+    "Anger": ["#550808", "#6E2C2C", "#6E0003"],
+    "Fear": ["#07461E", "#082D16", "#0E2D08"],
+    "Disgust": ["#5C2A9B", "#3E1C68", "#6C0DAC"],
+    "Joy": ["#939d7d"] //"#E9DD87", "#E9C873", "#E9CA0E"
   };
-  $("#particles-js").css("background-color", colorBackground[tone]);
-  $(document.body).css("background-color" , colorBackground[tone]);
+  var currentColor = $("#particles-js").css("background-color");
+  do {
+    var color = colorBackground[tone][Math.floor(Math.random() * colorBackground[tone].length)];
+  }
+  while (currentColor === color);
+  console.log(color);
+  $("#particles-js").css("background-color", color);
+  $(document.body).css("background-color" , color);
 }
 
 function separateStanzas(poemLinesArr) {
