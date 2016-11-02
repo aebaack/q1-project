a264dd46298d3fd93fcd63c57fbee90c20364970
Aidan Baack <aebaack@gmail.com>
2016-11-02 11:29:45 -0600

    Add ability to search by title, or title and author

--- js/script.js
+++ js/script.js
@@ -14,45 +14,49 @@ $("#searchPoems").on("click", function(event) {
   var poet = poemForm["poet"].value;
   var title = poemForm["title"].value;
   var length = poemForm["length"].value;
-  if (poet.length === 0 && title.length === 0 && length === "any") {
-    Materialize.toast('Enter more data!', 4000, 'red lighten-2');
-  } else {
-    var poem = $.getJSON("http://poetdb.herokuapp.com/author/"+poet);
-    var ul = $("#poemList");
-    $(ul).children().remove(); // Remove past searches from displaying upon a new search
-    $(".preloader-wrapper").removeClass("hide"); // Make sure the loader displays on a new search
-    poem.done(function(data) {
-      $(".preloader-wrapper").addClass("hide");
-      if (data.status) {
-        $("#notFound").removeClass("hide");
-        return;
-      }
-      $(ul).removeClass("hide");
-      $("#notFound").addClass("hide");
-      for (var i = 0; i < data.length; i++) {
-        if (data[i].lines.length > 500) {
-          continue;
-        }
-        var li = $("<li>");
-        var divHeader = $("<div>"), divBody = $("<div>");
-        divHeader.attr("class", "collapsible-header clearfix");
-        divHeader.append($("<div>").attr("class", "left").text(data[i].title)); divHeader.append($("<a>").attr("class", "waves-effect waves-light btn-flat red lighten-2 right poemSelector").attr("id", data[i].title).css("margin-top", "4px").text("Select"));
-        divBody.attr("class", "collapsible-body grey lighten-5");
-        var poemLines = "";
-        for (var j = 0; j < data[i].lines.length; j++) {
-          poemLines += data[i].lines[j] + "<br>";
-        }
-        $(divBody).html(poemLines);
-        li.append(divHeader).append(divBody);
-        ul.append(li);
-      }
-      $("#loadingPoems").text("Select a Poem");
-      $(".poemSelector").on("click", selectPoem);
-    });
-    poem.fail(function(err) {
-      console.log(err);
-    });
+
+  var poem;
+  if (poet.length && title.length) {
+      poem = $.getJSON("http://poetdb.herokuapp.com/title,author/"+title+";"+poet);
+  } else if (poet.length) {
+    poem = $.getJSON("http://poetdb.herokuapp.com/author/"+poet);
+  } else if (title.length) {
+    poem = $.getJSON("http://poetdb.herokuapp.com/title/"+title);
   }
+  var ul = $("#poemList");
+  $(ul).children().remove(); // Remove past searches from displaying upon a new search
+  $(".preloader-wrapper").removeClass("hide"); // Make sure the loader displays on a new search
+  poem.done(function(data) {
+    $(".preloader-wrapper").addClass("hide");
+    if (data.status) {
+      $("#notFound").removeClass("hide");
+      return;
+    }
+    $(ul).removeClass("hide");
+    $("#notFound").addClass("hide");
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].lines.length > 500) {
+        continue;
+      }
+      var li = $("<li>");
+      var divHeader = $("<div>"), divBody = $("<div>");
+      divHeader.attr("class", "collapsible-header clearfix");
+      divHeader.append($("<div>").attr("class", "left").text(data[i].title)); divHeader.append($("<a>").attr("class", "waves-effect waves-light btn-flat red lighten-2 right poemSelector").attr("id", data[i].title).css("margin-top", "4px").text("Select"));
+      divBody.attr("class", "collapsible-body grey lighten-5");
+      var poemLines = "";
+      for (var j = 0; j < data[i].lines.length; j++) {
+        poemLines += data[i].lines[j] + "<br>";
+      }
+      $(divBody).html(poemLines);
+      li.append(divHeader).append(divBody);
+      ul.append(li);
+    }
+    $("#loadingPoems").text("Select a Poem");
+    $(".poemSelector").on("click", selectPoem);
+  });
+  poem.fail(function(err) {
+    console.log(err);
+  });
 });
 
 function selectPoem(event) {
