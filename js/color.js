f998235cc0b1ef17a581bbea948a180d7fa2a18c
Aidan Baack <aebaack@gmail.com>
2016-11-03 14:32:15 -0600

    Add functionality to left and right arrow buttons

--- js/color.js
+++ js/color.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
   var analyzeText = stanzaData[1]; // String of stanzas formatted for tone analysis
 
   // Poem tone analysis
-  // var tones = $.getJSON("https://g-watson-aidanbaack.herokuapp.com/?text=" + analyzeText);
+  var tones = $.getJSON("https://g-watson-aidanbaack.herokuapp.com/?text=" + analyzeText);
   // var analyzeText = "This is a sentence.";
   // var tones = $.post("https://g-watson-aidanbaack.herokuapp.com/", {"text": analyzeText});
 
@@ -45,11 +45,6 @@ $(document).ready(function() {
     // stanzaToneList is an array of tone objects for each stanza
     stanzaToneList = toneData["sentences_tone"];
 
-    // Create particles JSON
-    // var numParticles = Math.floor(Math.random() * 50) + 15;
-
-    // var tempJSON = {"particles":{"number":{"value":24,"density":{"enable":true,"value_area":800}},"color":{"value":colors[strongestDocTone]},"shape":{"type":"polygon","stroke":{"width":0,"color":"#000"},"polygon":{"nb_sides":6},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.3,"random":true,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":31.565905665290902,"random":false,"anim":{"enable":true,"speed":10,"size_min":40,"sync":false}},"line_linked":{"enable":false,"distance":200,"color":"#ffffff","opacity":1,"width":2},"move":{"enable":true,"speed":8,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"roittateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"grab"},"onclick":{"enable":false,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true};
-
     particlesJS('particles-js', createParticlesObj(strongestDocTone)); // Display particles
     changeBackground(strongestDocTone, strongestDocTone); // Change background color to entire poem tone
   });
@@ -57,19 +52,35 @@ $(document).ready(function() {
   $(window).keydown(function(event) { // Keyboard controls
     switch (event.which) {
       case 37: // Left Arrow: Go one stanza back (if possible)
-        if (currentStanza > 0) {
-          currentStanza--;
-          changeStanzaBackground(); // Change background to current tone
-        }
+        previousStanza();
         break;
       case 39: // Right Arrow: Go one stanza forward (if possible)
-        if (currentStanza < (stanzas.length-1)) {
-          currentStanza++;
-          changeStanzaBackground(); // Change background to current tone
-        }
+        nextStanza();
         break;
     }
   });
+
+  $(".next").on("click", function() {
+    nextStanza();
+  });
+
+  $(".previous").on("click", function() {
+    previousStanza();
+  });
+
+  function previousStanza() {
+    if (currentStanza > 0) {
+      currentStanza--;
+      changeStanzaBackground(); // Change background to current tone
+    }
+  }
+
+  function nextStanza() {
+    if (currentStanza < (stanzas.length-1)) {
+      currentStanza++;
+      changeStanzaBackground(); // Change background to current tone
+    }
+  }
 
   function changeStanzaBackground() {
     // Change background color to match stanza tone and display the stanza
